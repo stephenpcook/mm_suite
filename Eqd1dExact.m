383e108cbe7f7ccf7d1d22aa690fd7683e6576b0
Stephen P. Cook <s.cook@bath.ac.uk>
2016-02-09 15:54:38 +0000

    Updated version from SISL for small gradients
    Better documentation.

--- Eqd1dExact.m
+++ Eqd1dExact.m
@@ -38,71 +38,44 @@ N=length(xin);
 h= 1./(N-1);      % Computational grid width
 xi= h*((1:N)-1)'; % Computational grid
 
-x0 = xin;   % Initial Physical Grid
+x0 = xin(:);   % Initial Physical Grid
 xp0= zeros(size(x0));
 
 % MAIN FUNCTIONALITY. WHAT A MESS!
 if strcmp(Monitor.type,'points')
     % Should I put a check in to see that the start and end points are the
     % same for x0 and monitor.x? Probably, not now.
-    x_m = Monitor.x;
-    Mx  = Monitor.M;
+    x_m = Monitor.x(:);
+    Mx  = Monitor.M(:);
     x = equidistribute(x0,x_m,Mx);
 elseif strcmp(Monitor.type,'function')
     Mx = Monitor.function(x0);
     x=equidistribute(x0,x0,Mx);
 end % if monitor.type==points
-xend = x;
+xend = reshape(x,size(xin));
 
 function xout = equidistribute(x0,x_m,Mx)
-    % PUT THIS INTO SUBFUNCTION. FOR ANALYTIC M CAN ITERATE THIS. (Converge?)
-    
-    % PART 1
-    if 0 % OLD CODE
-    %  % Integrate M over x_m by the composite Trapezium Rule
-    %  Nm = length(x_m);
-    %  IntMi = zeros(1,Nm-1); % Integral of each element
-    %  cIM = zeros(1,Nm-1); % Cumilitive Integral of M.
-    %  for ii = 1:Nm-1 
-    %    IntMi(ii) = 1/2*(Mx(ii)+Mx(ii+1))*(x_m(ii+1)-x_m(ii)); % Trapezium 
-    %  end % for ii
-    %  cIM=cumsum(IntMi); % Cumilitive integral, from 0 to x_m.
-    %  IntM = sum(IntMi); % Integral of whole interval. 
-    %  % AGAIN NEED TO BE CLEVERERER
+    % Find points xout that equidistribute the lin. interp. of (x_m,Mx)
+    %
+    %  IN
+    % x0  - the output will be the same size as x0 (length N)
+    % x_m - the knots of the Monitor function (X)
+    % Mx  - the values of the monitor function to be equidistributed to (U)
+    %  OUT
+    % xout - (Y)
 
-    %  % Place the points of x to satisty equidistribution.
-    %  xout = zeros(size(x0));
-    %  Nx = length(x0);
-    %  x_h = 1/(Nx-1);
-    %  % if (x(1) == x_m(1)) good, else do clever, end
-    %  xout(1) = x0(1);
-    %  Mi=1; % Index of cIM (left point), 1 to Nm-1
-    %  for ii = 2:N
-    %      Target = (ii-1)*x_h*IntM; % Target integral.
-    %      while and(Mi<Nm-1,cIM(Mi)<=Target); % Find the right interval.
-    %      				   % Mi is the index of interval and the
-    %      				   % left side interval
-    %          Mi = Mi + 1;
-    %      end % while
-    %      xL = x_m(Mi);
-    %      xR = x_m(Mi+1);
-    %      if Mi==1
-    %      ML=0;
-    %  else
-    %      ML = cIM(Mi-1);
-    %  end
-    %      M_in = IntMi(Mi);
-    %      xout(ii) = xL + (Target-ML)*(xR - xL)/(M_in);
-    %      s = (Mx(Mi+1) - Mx(Mi))/(xR-xL);
-    %      xout(ii) = xL + sqrt(2*(Target - ML)/s + (Mx(Mi)/s)^2) - Mx(Mi)/s;
-    %  end % for ii
-    end
+
     X = x_m;
     U = Mx;
     N = length(x0) - 1;
     
     Y = zeros(size(x0));
     
+    %   $$intUi(i) = \int_{X(i)}^{X(i+1)} u(x) dx ,$$
+    %   $$intU(i) = \int_{X(1)}^{X(i)} u(x) dx$$
+    % and
+    %   $$theta = \int_{X(1)}^{X(N)} u(x) dx$$
+    % Assuming U is piecewise linear.
     intUi = 1/2*(U(1:end-1)+U(2:end)).*(diff(X));
     intU = [0;cumsum(intUi(:))];
     theta = intU(end);
@@ -111,6 +84,7 @@ function xout = equidistribute(x0,x_m,Mx)
     jj = 1;
     for ii = 1:N-1
     Target = ii/N * theta;
+    % Find the interval in which Y(ii) lies.
     while and(intU(jj) < Target, jj<N)
         jj = jj + 1;
     end
@@ -126,11 +100,14 @@ function xout = equidistribute(x0,x_m,Mx)
     target_local = Target - intUL;
     m = (UR-UL)/(XR-XL);
     
-    if m==0
-      Y(ii+1) = XL + target_local/UL;
-    else
-      Y(ii+1) = XL + (-UL + sqrt(UL^2 + 2*m*target_local))/m;
-    end % if m==0
+    % In this interval 
+    %   $$U(x) = m*(x-XL) + UL .$$
+    % We want to find Y such that 
+    %   $$\int_XL^Y U(x) dx = target_local , $$
+    % so we integrate to get
+    %   $$ Y = XL + (-UL + sqrt(UL^2 + 2*m*target_local))/m . $$
+    % For small m, this gives huge error, so we rearrange as
+    Y(ii+1) = XL + 2*target_local/(UL + sqrt(UL^2 + 2*m*target_local));
     
     end % for ii
     Y(N+1) = X(N+1);
